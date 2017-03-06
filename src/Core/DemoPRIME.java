7a1d275a48bbb9566e5915973d18c59bda34b6ac
daddo <daddo@daddo-Extensa-2540>
2017-03-06 23:44:20 +0100

    Add package Gear

---
+++ src/Core/DemoPRIME.java
@@ -0,0 +1,21 @@
+package Core;
+
+public class DemoPRIME {
+	public boolean isPrime(int n) {
+		if(n>100) {
+			System.out.println("Il numero e' troppo grande");
+			return false;
+		}
+		int a=3;
+		if(n==2)
+			return true;
+		if(n%2==0)
+			return false;
+		while(a<n){
+			if(n%a==0)
+				return false;
+			a=a+2;
+		}
+		return true;
+	}
+}
---
+++ src/Gear/Animal.java
@@ -0,0 +1,5 @@
+package Gear;
+
+public class Animal {
+
+}
