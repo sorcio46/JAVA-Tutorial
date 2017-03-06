4b02a2c4824cc1439c8d8beae91412027f856e10
daddo <daddo@daddo-Extensa-2540>
2017-03-07 00:00:55 +0100

    Introduce Position

--- src/Gear/Animal.java
+++ src/Gear/Animal.java
@@ -1,5 +1,26 @@
 package Gear;
 
-public class Animal {
+import Utils.Position;
 
+public class Animal {
+	
+	private int health;
+	private Position position;
+	
+	Animal(Position p){
+		this.health=100;
+		this.position=p;
+	}
+	
+	public Position getPosition() {
+		return this.position;
+	}
+	
+	public void setPosition(Position p) {
+		this.position=p;
+	}
+	
+	public int getHealth() {
+		return this.health;
+	}
 }
---
+++ src/Utils/Position.java
@@ -0,0 +1,27 @@
+package Utils;
+
+public class Position {
+	private int x;
+	private int y;
+	
+	Position(int a, int b) {
+		this.x=a;
+		this.y=b;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+	
+	public void setX(int a){
+		this.x=a;
+	}
+	
+	public void setY(int a) {
+		this.y=a;
+	}
+}
